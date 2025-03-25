e74481d59ec30e0ef4486555e52cdbf3311060e2
BearToCode <davidebasso03@gmail.com>
2025-03-25 17:48:03 +0100

    complete task 2

--- lab01/main.m
+++ lab01/main.m
@@ -1,1 +1,14 @@
-fprintf("Project setup!\n");
+clc; clear;
+
+addpath(genpath('.'));
+
+params.density = 1000;
+params.h = 1;
+params.d = 1;
+params.g = 9.81;
+
+pendulums = sloshing_pendulums(params, 10);
+sys = sloshing_damped(pendulums, params, 0.003);
+
+impulse(sys, 10);
+grid on;
